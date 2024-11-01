f8fafdb3e5a1febddedfddabb1cdcf127359cc6d
Carlos Ruiz Herrera <carlos.ruiz.herrera@rock.estate>
2024-11-01 21:56:35 +0100

    Fix vs code repo

--- build/00-Base/build.sh
+++ build/00-Base/build.sh
@@ -13,4 +13,4 @@ wget https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-$REL
 wget https://copr.fedorainfracloud.org/coprs/tofik/nwg-shell/repo/fedora-$RELEASE/tofik-nwg-shell-fedora-$RELEASE.repo -O /etc/yum.repos.d/nwg-shell.repo
 
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
